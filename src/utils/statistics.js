df2020-sb/taskmanager-12

7.2.8 Настраиваем график по датам
- Опишем вспомогательные функции для подсчета
- Посчитаем и передадим в chart.js готовые данные

src/utils/statistics.js
@@ -12,8 +12,20 @@ export const colorToHex = {
 // Используем особенности Set, чтобы удалить дубли в массиве
 export const makeItemsUniq = (items) => [...new Set(items)];
 
+export const parseChartDate = (date) => moment(date).format(`D MMM`);
+
 export const countTasksByColor = (tasks, color) => {
   return tasks.filter((task) => task.color === color).length;
+};
+
+const isDueDateSame = (dateA, dateB) => moment(dateA).isSame(dateB);
+
+export const countTasksInDateRange = (dates, tasks) => {
+  return dates.map(
+      (date) => tasks.filter(
+          (task) => isDueDateSame(task.dueDate, date)
+      ).length
+  );
 };
 
 export const countCompletedTaskInDateRange = (tasks, dateFrom, dateTo) => {
@@ -34,4 +46,21 @@ export const countCompletedTaskInDateRange = (tasks, dateFrom, dateTo) => {
 
     return counter;
   }, 0);
+};
+
+export const getDatesInRange = (dateFrom, dateTo) => {
+  const dates = [];
+  let stepDate = new Date(dateFrom);
+
+  // Нам нужно получить все даты из диапазона,
+  // чтобы корректно отразить их на графике.
+  // Для этого проходим в цикле от даты "от"
+  // до даты "до" и каждый день, что между,
+  // заносим в результирующий массив dates
+  while (moment(stepDate).isSameOrBefore(dateTo)) {
+    dates.push(new Date(stepDate));
+    stepDate.setDate(stepDate.getDate() + 1);
+  }
+
+  return dates;
 };

VladislavYusupov/taskmanager-13

src/utils/statistics.js
@@ -1,8 +1,11 @@
 import dayjs from "dayjs";
 import isBetween from "dayjs/plugin/isBetween";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import {isDatesEqual} from "./task.js";
 import {Color} from "../const.js";
 
 dayjs.extend(isBetween);
+dayjs.extend(isSameOrBefore);
 
 export const colorToHex = {
   [Color.BLACK]: `#000000`,
@@ -15,8 +18,18 @@ export const colorToHex = {
 // Используем особенности Set, чтобы удалить дубли в массиве
 export const makeItemsUniq = (items) => [...new Set(items)];
 
+export const parseChartDate = (date) => dayjs(date).format(`D MMM`);
+
 export const countTasksByColor = (tasks, color) => {
   return tasks.filter((task) => task.color === color).length;
+};
+
+export const countTasksInDateRange = (dates, tasks) => {
+  return dates.map(
+      (date) => tasks.filter(
+          (task) => isDatesEqual(task.dueDate, date)
+      ).length
+  );
 };
 
 export const countCompletedTaskInDateRange = (tasks, dateFrom, dateTo) => {
@@ -37,4 +50,21 @@ export const countCompletedTaskInDateRange = (tasks, dateFrom, dateTo) => {
 
     return counter;
   }, 0);
+};
+
+export const getDatesInRange = (dateFrom, dateTo) => {
+  const dates = [];
+  let stepDate = new Date(dateFrom);
+
+  // Нам нужно получить все даты из диапазона,
+  // чтобы корректно отразить их на графике.
+  // Для этого проходим в цикле от даты "от"
+  // до даты "до" и каждый день, что между,
+  // заносим в результирующий массив dates
+  while (dayjs(stepDate).isSameOrBefore(dateTo)) {
+    dates.push(new Date(stepDate));
+    stepDate.setDate(stepDate.getDate() + 1);
+  }
+
+  return dates;
 };